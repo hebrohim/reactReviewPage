2041e117bc9d474e778a4e3aef8b85309e7ac53a
hebrohim <raheemibrohim@mail.com>
2023-03-13 23:33:03 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Review from "./components/Review";
+
+function App() {
+  return (
+    <div className="App">
+     <h2 id="reviewText">Reviews</h2>
+     <div id="underline"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Review.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Review from "./Data"
+
+const Review = () => {
+    c
+  return (
+    <div className='reviewContainer'>
+
+    </div>
+  )
+}
+
+export default Review
