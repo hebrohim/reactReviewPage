af1c014574cfb2396916ac2a7f92b6489732513c
hebrohim <raheemibrohim@mail.com>
2023-03-14 16:52:58 +0100

    feat:Add card with a review

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 
 import React from "react";
 import Review from "./components/Review";
-
+import Data from "./components/Data"
 function App() {
+
+  // console.log(Data)
   return (
     <div className="App">
      <h2 id="reviewText">Reviews</h2>
      <div id="underline"></div>
+
+     <Review  Reviews = {Data}/>
     </div>
   );
 }
--- src/components/Review.js
+++ src/components/Review.js
@@ -1,11 +1,23 @@
-import React from 'react'
-import Review from "./Data"
+import React, { useState } from 'react'
+import { BiChevronRight,BiChevronLeft } from "react-icons/bi";
 
-const Review = () => {
-    c
+const Review = ({Reviews}) => {
+const [index, setindex] = useState(0);
+let {id,name,job,image,text} = Reviews[index]
+console.log(id,name,job)
   return (
     <div className='reviewContainer'>
+        <img id = "image" src={image} alt ={name}/>
+        <h4 id='author'>{name}</h4>
+        <p id='jobTitle'>{job}</p>
+        <p id='info'>{text}</p>
 
+<div style={{display:"flex"}}>
+        <span id='previousBtn'><BiChevronLeft/></span>
+        <span id='nextBtn'><BiChevronRight/></span>
+        </div>
+
+        <button id = "randomBtn">Random</button>
     </div>
   )
 }
