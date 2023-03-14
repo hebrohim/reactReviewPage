b99b40f1db521fbd02f7ef9a9ae8e7403e03919a
hebrohim <raheemibrohim@mail.com>
2023-03-14 21:55:03 +0100

    fix:allow user move to the first testimonial after clicking on the last testimonial

--- src/components/Review.js
+++ src/components/Review.js
@@ -4,15 +4,23 @@ import { FaQuoteRight } from "react-icons/fa";
 const Review = ({Reviews}) => {
 const [index, setindex] = useState(0);
 let {id,name,job,image,text} = Reviews[index]
-console.log(id,name,job)
+// console.log(id,name,job)
 const next = () =>{
   var add = index + 1
   setindex(add)
+  if ( index >= Reviews.length - 1) {
+  setindex(0)
+}
 }
 
 const previous = () =>{
   setindex(index - 1)
+  if ( index <= 0) {
+   setindex(Reviews.length - 1)
+    }
 }
+
+
   return (
     <div className='reviewContainer'>
       <span id='quote'><FaQuoteRight /></span>
