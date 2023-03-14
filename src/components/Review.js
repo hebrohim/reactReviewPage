144a6fcf1f9817f5735cbd1e5acb546d36e471d5
hebrohim <raheemibrohim@mail.com>
2023-03-14 20:10:32 +0100

    feat:create a button to switch to the next and previous author

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react'
 import { BiChevronRight,BiChevronLeft } from "react-icons/bi";
-
+import { FaQuoteRight } from "react-icons/fa";
 const Review = ({Reviews}) => {
 const [index, setindex] = useState(0);
 let {id,name,job,image,text} = Reviews[index]
 console.log(id,name,job)
+const next = () =>{
+  var add = index + 1
+  setindex(add)
+}
+
+const previous = () =>{
+  setindex(index - 1)
+}
   return (
     <div className='reviewContainer'>
-        <img id = "image" src={image} alt ={name}/>
+      <span id='quote'><FaQuoteRight /></span>
+       <div> <img id = "image" src={image} alt ={name}/></div>
         <h4 id='author'>{name}</h4>
         <p id='jobTitle'>{job}</p>
         <p id='info'>{text}</p>
 
 <div style={{display:"flex"}}>
-        <span id='previousBtn'><BiChevronLeft/></span>
-        <span id='nextBtn'><BiChevronRight/></span>
+        <button id='previousBtn' onClick={previous}><BiChevronLeft/></button>
+        <button id='nextBtn' onClick={next}><BiChevronRight/></button>
         </div>
 
         <button id = "randomBtn">Random</button>
